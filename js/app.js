f486689b0e9ad5da99c5729b07311eb187f07941
Dize dev <axiozweb2022@gmail.com>
2024-07-21 22:33:00 -0500

    se creo la configuracion del scroll smooth

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 import { DATA } from './data.js';
-import { create, select } from './helper.js';
+import { create, select, selectAll, SmoothScollConfig } from './helper.js';
 
 const PADDING = 20;
 const ELEMENT_ZIZE = 170;
 
 window.addEventListener('load', () => {
 	const items = select('.item');
+	const circle = select('.cursor-circle');
+
+	const scroll = { current: 0, target: 0 };
+
+	const mouseEvent = { clientX: 0, clientY: 0 };
 
 	const generateList = () => {
 		DATA.forEach((d, index) => {
@@ -34,7 +39,51 @@ window.addEventListener('load', () => {
 	};
 	generateList();
 
-	const init = () => {};
+	const list = selectAll('.text-container');
+
+	const updateTarget = (event) => (scroll.target += event.deltaY);
+
+	const updateMouseEvent = (event) => {
+		mouseEvent.clientX = event.clientX;
+		mouseEvent.clientY = event.clientY;
+	};
+
+	document.addEventListener('wheel', updateTarget);
+
+	document.addEventListener('mousemove', updateMouseEvent);
+
+	const init = () => {
+		const { current, target } = scroll;
+		const transition = SmoothScollConfig.lerp(current, target);
+
+		scroll.current = transition;
+
+		const skew = SmoothScollConfig.getSkew(target, current);
+
+		gsap.to(circle, {
+			top: mouseEvent.clientY - 8.5,
+			left: mouseEvent.clientX - 8.5,
+			duration: 0.3,
+			className: 'cursor-circle',
+		});
+
+		list.forEach((item) => {
+			const y = gsap.getProperty(item, 'y');
+
+			const size = (ELEMENT_ZIZE + PADDING) * DATA.length;
+
+			const scrollY = y - scroll.current;
+
+			let result = (scrollY % size) - (ELEMENT_ZIZE + PADDING);
+
+			if (result < -231)
+				result = (scrollY % size) + size - (ELEMENT_ZIZE + PADDING);
+
+			item.style.transform = `translateY(${result}px) skewY(${skew}deg)`;
+		});
+
+		window.requestAnimationFrame(init);
+	};
 
 	init();
 });
--- js/helper.js
+++ js/helper.js
@@ -2,4 +2,23 @@ const select = (element) => document.querySelector(element);
 const selectAll = (element) => document.querySelectorAll(element);
 const create = (element) => document.createElement(element);
 
-export { select, selectAll, create };
+class SmoothScollConfig {
+	static ease = 0.15;
+
+	static lerp(current, target) {
+		const distanceBetewen = target - current;
+		const distanceToTravel = distanceBetewen * this.ease;
+
+		return current + distanceToTravel;
+	}
+
+	static getSkew(target, current) {
+		const diff = target - current;
+		const aceleration = diff / window.innerWidth;
+		const velocity = +aceleration;
+
+		return velocity * 1.5;
+	}
+}
+
+export { select, selectAll, create, SmoothScollConfig };
