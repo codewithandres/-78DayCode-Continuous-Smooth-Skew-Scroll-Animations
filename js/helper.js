da2713fa1afaeec31ccd5073eb9893cf4b1145cd
Dize dev <axiozweb2022@gmail.com>
2024-07-21 20:37:38 -0500

    funciones de ayuda para seleccionar elementos del html

---
+++ js/helper.js
@@ -0,0 +1,5 @@
+const select = (element) => document.querySelector(element);
+const selectAll = (element) => document.querySelectorAll(element);
+const create = (element) => document.createElement(element);
+
+export { select, selectAll, create };
